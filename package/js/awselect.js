28e5723fb784a66800baa214d227efb058938718
Previnash Wong <prevwong@Prevs-MBP.local>
2016-12-28 01:04:25 +0800

    Fixed deanimation speed

--- package/js/awselect.js
+++ package/js/awselect.js
@@ -139,9 +139,9 @@ awselect_count = 0; // used for generating sequential ID for <select> that does 
         $(awselect).each(function() {
             var element = $(this);
             if (element.hasClass("animate")) {
-                setTimeout(function() {
+                //setTimeout(function() {
                     element.removeClass("animate2");
-                    setTimeout(function() {
+                    //setTimeout(function() {
                         element.find(".back_face").hide();
                         element.removeClass("animate");
                         switchPlaceholderColor(element);
@@ -152,8 +152,8 @@ awselect_count = 0; // used for generating sequential ID for <select> that does 
                         setTimeout(function() {
                             element.removeClass("placeholder_animate");
                         }, 100);
-                    }, 200);
-                }, 400);
+                    //}, 0);
+                //}, 0);
             }
         });
     }
@@ -177,7 +177,7 @@ awselect_count = 0; // used for generating sequential ID for <select> that does 
         }
         setTimeout(function() {
             deanimate();
-        }, 200);
+        }, 100);
     }
     function icon(color) {
         return '<svg style="fill:' + color + '" version="1.1" id="Chevron_thin_down" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve"><path d="M17.418,6.109c0.272-0.268,0.709-0.268,0.979,0c0.27,0.268,0.271,0.701,0,0.969l-7.908,7.83c-0.27,0.268-0.707,0.268-0.979,0l-7.908-7.83c-0.27-0.268-0.27-0.701,0-0.969c0.271-0.268,0.709-0.268,0.979,0L10,13.25L17.418,6.109z"/></svg>';
